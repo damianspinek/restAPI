b6917ca88036b3db247f96f3ad87582dfe83ad70
Damian Spinek <damian00737@gmail.com>
2015-11-30 21:35:59 +0100

    add download file method from library1

--- src/main/java/pl/ppkwu/restapi/RestApi.java
+++ src/main/java/pl/ppkwu/restapi/RestApi.java
@@ -1,15 +1,26 @@
 package pl.ppkwu.restapi;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import library1.FileDownloader;
 
 @RestController
 public class RestApi {
 
 	@RequestMapping(value = "/download", params = {"url"})
 	public HttpEntity<byte[]> downloadFile(@RequestParam("url") String url) {
+		FileDownloader.downloadFile(url, "plik");
 		return null;
 	}
+	 
 }
