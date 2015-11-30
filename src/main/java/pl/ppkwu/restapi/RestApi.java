3bae29ea896aec61461c5f4f67cc595f780da03e
Damian Spinek <damian00737@gmail.com>
2015-11-30 21:11:28 +0100

    Add method to download file

--- src/main/java/pl/ppkwu/restapi/RestApi.java
+++ src/main/java/pl/ppkwu/restapi/RestApi.java
@@ -1,8 +1,13 @@
 package pl.ppkwu.restapi;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApi {
 
+	public HttpEntity<byte[]> downloadFile(@RequestParam("url") String url) {
+		return null;
+	}
 }
