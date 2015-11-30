dc99ab068da2ddbcfc7c5a84c6e7e04e645a8649
Damian Spinek <damian00737@gmail.com>
2015-11-30 21:14:01 +0100

    Add mapping to method

--- src/main/java/pl/ppkwu/restapi/RestApi.java
+++ src/main/java/pl/ppkwu/restapi/RestApi.java
@@ -1,12 +1,14 @@
 package pl.ppkwu.restapi;
 
 import org.springframework.http.HttpEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RestApi {
 
+	@RequestMapping(value = "/download", params = {"url"})
 	public HttpEntity<byte[]> downloadFile(@RequestParam("url") String url) {
 		return null;
 	}
