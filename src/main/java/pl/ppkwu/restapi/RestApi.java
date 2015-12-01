1f760ce9d89f394e78b6573125425ec115c5f9e9
Damian Spinek <damian00737@gmail.com>
2015-12-01 17:47:15 +0100

    fix unzip

--- src/main/java/pl/ppkwu/restapi/RestApi.java
+++ src/main/java/pl/ppkwu/restapi/RestApi.java
@@ -51,7 +51,7 @@ public class RestApi {
 		}
 	}
 	
-	@RequestMapping(value = "uznip", method = RequestMethod.GET)
+	@RequestMapping(value = "/uznip", method = RequestMethod.GET)
     public String unzip() {
         return "<form action=\"/unzip\" enctype=\"multipart/form-data\" method=\"post\">\n" +
                 "  <input type=\"file\" name=\"file\">\n" +
